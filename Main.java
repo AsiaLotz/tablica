90d01413235dfa05774a7dbe32dd024c2bb3d2fe
AsiaLotz <67192498+AsiaLotz@users.noreply.github.com>
2022-10-13 14:38:02 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static int[] sortNumbers(int[] numbers)
+    {
+
+        int length = numbers.length;
+
+        for (int i = 0; i < length - 1; i++) {
+
+            if (numbers[i] < numbers[i + 1]) {
+
+
+                int temp = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
+
+                i = -1;
+            }
+        }
+
+        return numbers;
+    }
+
+
+    private static boolean search(int[] numbers, int x) {
+        for (int i : numbers) {
+            if (i == x) {
+                System.out.printf("Found number " + x + "so it's true!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {7,8,10,34,28,65,99,4};
+
+        numbers = sortNumbers(numbers);
+
+        System.out.println("Sorted array: "
+                + Arrays.toString(numbers));
+
+        search(numbers,4);
+        }
+
+    }
+
+
+
